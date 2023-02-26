181f2eee54f20adf0f4107190f98530f3462f473
ankit12mz <100269663+ankit12mz@users.noreply.github.com>
2023-02-26 12:16:57 +0530

    Update test6_uniq_custom.cpp

--- test6_uniq_custom.cpp
+++ test6_uniq_custom.cpp
@@ -1,78 +1,76 @@
 #include <iostream>
-
+#include "header.h"
 using namespace std;
 
 template <typename T>
 class unique{
 	T *ptr;
 	public:
-		unique(): ptr(nullptr){			
-		}
-		
-		explicit unique(T *t):ptr(t){
-		}
-		
-		unique(const unique &obj) = delete;
-		unique& operator=(const unique &obj) = delete;
-		
-		unique(unique &&obj){
-			ptr = obj.release();
-		}
-		unique& operator=(unique &&obj){
-			ptr = obj.release();
-			return ptr;
-		}
-		
-		T& operator *(){
-			return *ptr;	
-		}
-		
-		T operator->(){
-			return ptr;
-		}
-		
-		explicit operator bool() const{
-			return ptr;
-		}
-		
+		unique():ptr(nullptr) {}
+		explicit unique(T *p): ptr(p) {}
 		~unique(){
-			delete ptr;	
+			delete ptr;
+			ptr = nullptr;
+		}
+		unique(const unique &obj) = delete;
+		unique& operator=(const unique &obj)=delete;
+		
+		unique(unique &&obj):ptr(obj.release()){
+//			ptr = obj.release();
+		}
+	
+		unique& operator = (unique &&obj){
+				ptr = obj.release();
+				return *this;
+		}
+	
+		T* get(){
+			return ptr;	
 		}
 		
 		T* release(){
-			T *temp= ptr;
-			ptr= nullptr;
+			T *temp = ptr;
+			ptr = nullptr;
 			return temp;
 		}
-		
-		void reset(T *t){
-			if(t!=ptr)
-				release();
-			ptr = t;
+	
+	
+		void reset(T *p=nullptr){
+			if(ptr !=p)
+				delete ptr;
+			ptr = p;
 		}
+		
+		T& operator*(){
+			return *ptr;
+		}
+		T* operator->(){
+			return ptr;
+		}
+		explicit operator bool(){
+			return ptr!=nullptr;
+		}
+
+	
 	
 };
 
-class Integer{
-	public:
-		int val;
-		Integer(int n):val(n){
-		}
-		int get(){
-			return val;
-		}
-};
+unique<int> Print(unique<int> obj1){
+//	*obj1 += *obj1;
+	*obj1 += *obj1;
+	cout<<*obj1<<endl;
+//	
+	unique<int> temp = move(obj1);
+	return temp;
+//	return nullptr;
+}
 
 int main(){
+	unique<int> obj1(new int(8));
+	unique<int> obj2 = (move(obj1)) ;
+	cout<<*obj2<<endl;
 	
-	unique<Integer> ob1(new Integer(3));
-
-	cout<< """"<<endl;
-	
-	
-	if(!ob1){
-		cout<<"unique pointer empty"<<endl;
-	}
-	
+	obj1 = Print (move(obj2));
+	cout<<"*obj1: "<<*obj1<<endl;
 	return 1;
 }
